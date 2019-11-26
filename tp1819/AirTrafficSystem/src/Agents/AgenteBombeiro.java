1b95eb6303a6a7ae90bf465de4437c94e3b5f29d
Vitor Castro <v.j.r.c1997@hotmail.com>
2019-11-26 17:43:11 +0000

    Start Movementon Bombeiro

--- tp1819/AirTrafficSystem/src/Agents/AgenteBombeiro.java
+++ tp1819/AirTrafficSystem/src/Agents/AgenteBombeiro.java
@@ -17,6 +17,8 @@ public class AgenteBombeiro extends Agent {
     private String id;
     private int x, y;
     private int xActiveFire, yActiveFire;
+    private int direcaoX = 0, direcaoY = 0;
+    private int velocidadeMax;
     private boolean active, replenishment;
     // listar em relação à lista de informação de aviões
     private DFAgentDescription dfdEstacoes, dfdBombeiro;
@@ -38,6 +40,11 @@ public class AgenteBombeiro extends Agent {
         int identificador = (Integer) args[0]; // id único
         int type = (Integer) args[1]; // tipo de bombeiro (1-aeronave, 2-drone, 3-camioes)
         id = "type" + Integer.toString(type) + "id" + Integer.toString(identificador);
+
+        // adapt velocidadeMax according to vehicle type
+        if(true) { // THIS HAS TO CHANGE
+            velocidadeMax = 5;
+        }
 
         // definições de DF
         // Para comunicação com estações
@@ -141,6 +148,8 @@ public class AgenteBombeiro extends Agent {
                     xActiveFire = Integer.parseInt(coordinates[0]);
                     yActiveFire = Integer.parseInt(coordinates[1]);
                     System.out.println("Fogo em (" + xActiveFire + ", " + yActiveFire + ")");
+                    // definir direção no eixo para luta a incêndio
+                    adaptDirectionOfMovement(xActiveFire, yActiveFire);
                     // reply
                     ACLMessage response = msg.createReply();
                     response.setPerformative(ACLMessage.CONFIRM);
@@ -152,5 +161,95 @@ public class AgenteBombeiro extends Agent {
 
         }
 
+        private void adaptDirectionOfMovement(int xFire, int yFire) {
+
+            if (xFire > x) {
+                direcaoX = 1;
+            }
+            else {
+                direcaoX = -1;
+            }
+            if (yFire > y) {
+                direcaoY = 1;
+            }
+            else {
+                direcaoY = -1;
+            }
+        }
+    }
+
+    // simular movimento do bombeiro de acordo com a sua velocidade
+    private class Movimento extends TickerBehaviour {
+        private int velocidadeAtual = 0;
+
+        public Movimento(Agent a, long period) { super(a, period); }
+        protected void onTick() {
+            if(velocidadeAtual!=0) {
+                x=x+velocidadeMax*direcaoX;
+                y=y+velocidadeMax*direcaoY;
+
+
+                // ESTOU AQUI!!!
+
+                // informação para interface
+                try {
+                    DFAgentDescription d = new DFAgentDescription();
+                    ServiceDescription s = new ServiceDescription();
+                    s.setType("Interface");
+                    d.addServices(s);
+                    // procurar interfaces registadas
+                    DFAgentDescription[] r = DFService.search(this.myAgent, d);
+                    if (r.length > 0) {
+                        // enviar mensagem com nome, coordX e coordY para todas as interfaces registadas
+                        for (int i = 0; i < r.length; ++i) {
+                            DFAgentDescription d2 = r[i];
+                            AID p = d2.getName();
+                            ACLMessage m = new ACLMessage(ACLMessage.INFORM);
+                            m.addReceiver(p);
+                            m.setContent(name+";"+coordX+";"+coordY);
+                            send(m);
+                        }
+                    }
+                } catch (FIPAException fe) {
+                    fe.printStackTrace();
+                }
+                distPercorrida+=velocidadeAtual;
+                distPercorrer-=velocidadeAtual;
+                tempoDecorrido++;
+                tempoFalta=distPercorrer/velocidadeAtual;
+                if(alterouDir || alterouVel) {
+                    // informar estação seguinte das suas novas condições de viagem
+                    try {
+                        DFAgentDescription dfd = new DFAgentDescription();
+                        ServiceDescription sd = new ServiceDescription();
+                        sd.setType(aes.get(conta+1)); // proxima estação (para a qual se viaja agora)
+                        dfd.addServices(sd);
+                        // procurar estações registadas com aquele nome (deverá ser apenas 1)
+                        DFAgentDescription[] results = DFService.search(this.myAgent, dfd);
+                        if (results.length > 0) {
+                            // enviar mensagem com nome, tempo decorrido da viagem e tempo que falta para chegar
+                            for (int i = 0; i < results.length; ++i) {
+                                DFAgentDescription dfd2 = results[i];
+                                AID provider = dfd2.getName();
+                                ACLMessage msg2 = new ACLMessage(ACLMessage.INFORM_IF);
+                                msg2.addReceiver(provider);
+                                if(alterouDir) {
+                                    distPercorrer=Math.sqrt(((Math.pow((destCoordX - coordX), 2)) + (Math.pow((destCoordY - coordY), 2))));
+                                    direcaoX=(destCoordX-coordX)/distPercorrer;
+                                    direcaoY=(destCoordY-coordY)/distPercorrer;
+                                    tempoFalta=distPercorrer/velocidadeAtual;
+                                }
+                                msg2.setContent(name+";"+tempoDecorrido+";"+tempoFalta);
+                                send(msg2);
+                            }
+                        }
+                    } catch (FIPAException fe) {
+                        fe.printStackTrace();
+                    }
+                    alterouDir=false;
+                    alterouVel=false;
+                }
+            }
+        }
     }
 }
